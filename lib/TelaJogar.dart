1ef9e7df7ad3405e28e38b99efc7238dc66dee12
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-28 14:05:55 -0400

    Criando um app cara ou coroa, criamos apenas criamos o esqueleto da TelaJogar

---
+++ lib/TelaJogar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TelaJogar extends StatefulWidget {
+  const TelaJogar({super.key});
+
+  @override
+  State<TelaJogar> createState() => _TelaJogarState();
+}
+
+class _TelaJogarState extends State<TelaJogar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cara_coroa/TelaJogar.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+    home: TelaJogar(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
